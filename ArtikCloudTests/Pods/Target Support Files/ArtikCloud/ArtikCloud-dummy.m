9abcf6abf1830138037bcfed4cabc0aeb9832650
Jeanine Jue <jswattonjue@gmail.com>
2017-01-07 18:17:39 -0800

    Test changes

---
+++ ArtikCloudTests/Pods/Target Support Files/ArtikCloud/ArtikCloud-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ArtikCloud : NSObject
+@end
+@implementation PodsDummy_ArtikCloud
+@end
---
+++ ArtikCloudTests/Pods/Target Support Files/Pods-ArtikCloudClientTests/Pods-ArtikCloudClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArtikCloudClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_ArtikCloudClientTests
+@end
